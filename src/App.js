266104e3ca63ca04b9e0f5b4b03c81b623a31031
Sean R Reid <sean@torchcodelab.com>
2020-10-14 13:57:38 -0400

    Added loadData() function

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import React, { Component } from 'react';
-import UserCardList from './components/UserCardList';
+import React, { Component } from "react";
+import UserCardList from "./components/UserCardList";
 
 class App extends Component {
   state = {
-    userName: '',
-    users: []
+    userName: "",
+    users: [],
   };
 
-  handleSubmit = async event => {
+  loadData = async (userName) => {
+    const response = await fetch(`https://api.github.com/users/${userName}`);
+    const data = await response.json();
+    return data;
+  };
+
+  handleSubmit = async (event) => {
     event.preventDefault();
 
     const { userName } = this.state;
-    const response = await fetch(`https://api.github.com/users/${userName}`);
-    const data = await response.json();
 
-    if (data.message !== 'Not Found') {
+    const data = await this.loadData(userName);
+
+    if (data.message !== "Not Found") {
       this.setState({
         users: [...this.state.users, data],
-        userName: ''
+        userName: "",
       });
     } else {
       this.setState({
-        userName: ''
+        userName: "",
       });
     }
   };
 
-  handleChange = event => {
+  handleChange = (name) => {
     this.setState({
-      userName: event.target.value
+      userName: name,
     });
   };
 
@@ -36,14 +42,16 @@ class App extends Component {
     const { userName, users } = this.state;
     return (
       <>
-        <form onSubmit={this.handleSubmit}>
+        <form>
           <input
             type="text"
             value={userName}
-            onChange={this.handleChange}
+            onChange={(event) => this.handleChange(event.target.value)}
             placeholder="Search for a username..."
           />
-          <button type="submit">Add User Card</button>
+          <button type="button" onClick={this.handleSubmit}>
+            Add User Card
+          </button>
         </form>
         <UserCardList users={users} />
       </>

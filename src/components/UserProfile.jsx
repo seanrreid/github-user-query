0f4b57e4eef7d285870904a14b15fadbbb1726c4
Sean Reid <sean@torchcodelab.com>
2021-07-23 09:50:24 -0400

    updating react router hooks

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,50 +1,47 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const UserProfile = (props) => {
-  const { users } = props;
-  const { userName } = useParams();
+    const { users } = props;
+    const { userName } = useParams();
 
-  const user = users.find((user) => {
-    return user.login === userName ? user : null;
-  });
+    const user = users.find((user) => {
+        return user.login === userName ? user : null;
+    });
 
-  // Get the repos for the username in the params:
-  const [repoList, setRepos] = useState([]);
+    // Get the repos for the username in the params:
+    //   const [repoList, setRepos] = useState([]);
 
-  useEffect(() => {
-    (async function () {
-      const response = await fetch(
-        `https://api.github.com/users/${userName}/repos`
-      );
-      const repoList = await response.json();
-      setRepos(repoList);
-    })();
-  }, [setRepos, userName]);
+    //   useEffect(() => {
+    //     (async function () {
+    //       const response = await fetch(
+    //         `https://api.github.com/users/${userName}/repos`
+    //       );
+    //       const repoList = await response.json();
+    //       setRepos(repoList);
+    //     })();
+    //   }, [setRepos, userName]);
 
-  return (
-    <>
-      {!!user ? (
+    return (
         <>
-          <h2>{user.name}</h2>
-          <h3>{user.company}</h3>
-          <p>{user.bio}</p>
-          <p>Followers: {user.followers}</p>
-          {!!repoList ? (
-            <ul>
-              {repoList.map((repo) => (
-                <li key={repo.id}>{repo.name}</li>
-              ))}
-            </ul>
-          ) : (
-            <p>Loading repos...</p>
-          )}
+            {!!user ? (
+                <>
+                    <h2>{user.name}</h2>
+                    <h3>{user.company}</h3>
+                    <p>{user.bio}</p>
+                    <p>Followers: {user.followers}</p>
+                    {/* <ul>
+                      {!!repoList ??
+                          repoList.map((repo) => (
+                              <li key={repo.id}>{repo.name}</li>
+                          ))}
+                    </ul> */}
+                </>
+            ) : (
+                <p>Users array is empty</p>
+            )}
         </>
-      ) : (
-        <p>Users array is empty</p>
-      )}
-    </>
-  );
+    );
 };
 
 export default UserProfile;
